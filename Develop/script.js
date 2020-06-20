a5021c29d4f162826ba8e3d3de635ab7e5ea88e4
Brenna Good <brenna.elise.good@gmail.com>
2020-06-19 20:33:51 -0700

    Progress for week 3 Homework

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// This is my prompts for the password
 function generatePassword() {
   alert ("Please choose your password criteria from the following");
   var lengthPassword = prompt("Please select your password length, it must be between 8-128 characters");
@@ -17,7 +18,28 @@ function generatePassword() {
   var wantUpperCase = confirm("Do you want an uppercase character?");
   var wantNumbers = confirm("Do you want a number?");
 
+  // This is my list of characters 
+
+  var characters = ["!@#$%^&*", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890"];
+
+  // This is my if/else options
+
+  if (wantSpecialCharacter) {
+    masterString += characters[0];
+  }
+  if (wantLowerCase) {
+    masterString += characters[1];
+  }
+  if (wantUpperCase) {
+    masterString += characters[2];
+  }
+  if (wantNumbers) {
+    masterString += characters[3]
+  }
+  
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
